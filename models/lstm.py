eebb514347ce2d623b1afffebfb2c35f9c9ad443
nikunj-indoriya <170117784+nikunj-indoriya@users.noreply.github.com>
2026-03-29 23:07:55 +0530

    Added LSTM model

--- models/lstm.py
+++ models/lstm.py
@@ -1,13 +1,13 @@
 import torch.nn as nn
 
-class RNNLM(nn.Module):
+class LSTMLM(nn.Module):
     def __init__(self, vocab_size, embed_dim, hidden_dim):
         super().__init__()
         self.embedding = nn.Embedding(vocab_size, embed_dim)
-        self.rnn = nn.RNN(embed_dim, hidden_dim, batch_first=True)
+        self.lstm = nn.LSTM(embed_dim, hidden_dim, batch_first=True)
         self.fc = nn.Linear(hidden_dim, vocab_size)
 
     def forward(self, x):
         x = self.embedding(x)
-        out, _ = self.rnn(x)
+        out, _ = self.lstm(x)
         return self.fc(out)
